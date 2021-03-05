e513ef8a42aeb89613c0c8c26fc054ee79538b27
pandabeards <mrztryan@gmail.com>
2021-03-05 11:27:39 -0500

    initial code for tasks

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -7,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
   taskStatus = true;
+  loadedTasks: Task[] = [];
+  
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getTasks();
   }
 
+  createTask(taskData: Task) {
+    this.http
+      .post<{ name: string }>(
+        'https://moment-lite-default-rtdb.firebaseio.com/task.json',
+        taskData
+      ).subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  private getTasks() {
+    this.http
+      .get<{ [key: string]: Task }>('https://moment-lite-default-rtdb.firebaseio.com/task.json')
+      .pipe(
+        map(responseData => {
+          const tasksArray: Task[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              tasksArray.push({ ...responseData[key], id: key })
+            }
+          }
+          return tasksArray;
+        })
+      )
+      .subscribe(tasks => {
+        this.loadedTasks = tasks;
+      });
+  }
 }
