6d8bcf3f6bfbf89a8ab236f79fc95f6648fcc5c4
pandabeards <mrztryan@gmail.com>
2021-03-07 15:27:06 -0500

    env setup for backend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "backend_user",
-  PASSWORD: "osc4rm3y3r",
-  DB: "moment_db",
+  HOST: process.env.DBHOST,
+  USER: process.env.DBUSER,
+  PASSWORD: process.env.DBPASS,
+  DB: process.env.DB,
   dialect: "postgres",
   pool: {
     max: 5,
